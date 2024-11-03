6665202b869d2b2a44112d83ee0a13fd78e2a1b2
Joaomarcellodev <joaomarcellobraz@gmail.com>
2024-11-03 19:20:14 -0300

    Adicionado o titulo + mensagem principal do menu e inicio da construcao de decisao

---
+++ menu.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main(){
+
+    int op;
+
+    // Mensagem principal
+    printf("=== MENU PRINCIPAL ===\n\n");
+
+    // Opcoes do Menu
+    printf("1. Calcular Area do Triangulo \n");
+    printf("2. Calcular Area do Retangulo \n");
+    printf("3. Indentificar o Tipo de Retangulo \n");
+    printf("4. Imprimir Numeros de Vetor \n");
+    printf("5. Sair \n\n");
+
+    // Mensagem para o usuario informar a opcao desejada
+    printf("Informe a opcao desejada: \n");
+    scanf("%d",&op);
+
+    // Estrutura de decisao - Para a escolha do menu
+
+    switch (op)
+    {
+    case 1:
+        
+        break;
+    
+    default:
+        break;
+    }
+
+
+
+}
